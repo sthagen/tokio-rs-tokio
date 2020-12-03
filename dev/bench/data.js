62c50e9dad067049112d4e94aa977e183df5961b
github-action-benchmark <github@users.noreply.github.com>
2020-12-03 12:37:12 +0000

    add sync_mpsc (cargo) benchmark result for 6c4a677f4bb8a22fe6680f4b5ca56e65cd7c99ae

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1606998949877,
+  "lastUpdate": 1606999032027,
   "repoUrl": "https://github.com/sthagen/tokio",
   "entries": {
     "rt_multi_threaded": [
@@ -47,6 +47,92 @@ window.BENCHMARK_DATA = {
             "name": "yield_many",
             "value": 17341859,
             "range": "± 3561894",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ],
+    "sync_mpsc": [
+      {
+        "commit": {
+          "author": {
+            "email": "stefan@hagen.link",
+            "name": "Stefan Hagen",
+            "username": "sthagen"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6c4a677f4bb8a22fe6680f4b5ca56e65cd7c99ae",
+          "message": "Merge pull request #25 from tokio-rs/master\n\nSync Fork from Upstream Repo",
+          "timestamp": "2020-12-03T12:57:13+01:00",
+          "tree_id": "1a4d4bcc017f6a61a652505b1edd4a3bf36ea1ab",
+          "url": "https://github.com/sthagen/tokio/commit/6c4a677f4bb8a22fe6680f4b5ca56e65cd7c99ae"
+        },
+        "date": 1606999031215,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "contention_bounded",
+            "value": 6633980,
+            "range": "± 2035354",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "contention_bounded_full",
+            "value": 6925979,
+            "range": "± 1917747",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "contention_unbounded",
+            "value": 6258211,
+            "range": "± 2453073",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_100_000_medium",
+            "value": 695,
+            "range": "± 5",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_100_medium",
+            "value": 694,
+            "range": "± 7",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_1_medium",
+            "value": 694,
+            "range": "± 28",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "send_large",
+            "value": 49906,
+            "range": "± 1111",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "send_medium",
+            "value": 827,
+            "range": "± 12",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontented_bounded",
+            "value": 1121654,
+            "range": "± 119063",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontented_unbounded",
+            "value": 792616,
+            "range": "± 7406",
             "unit": "ns/iter"
           }
         ]
